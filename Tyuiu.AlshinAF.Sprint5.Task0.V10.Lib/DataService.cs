bb59c182245fe43386408fb0f08f41ef6317a670
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-28 20:14:15 +0500

    Add project files.

---
+++ Tyuiu.AlshinAF.Sprint5.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.IO.Compression;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AlshinAF.Sprint5.Task0.V10.Lib
+{
+    public class DataService : ISprint5Task0V10
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double z = (1.6 * Math.Pow((double)x, 3) - 2.1 * Math.Pow((double)x, 2) + 7 * x);
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
