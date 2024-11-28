060ef420068cc14f7d6d954c0980bfea2626ca1e
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-28 20:20:24 +0500

    Task0

--- Tyuiu.AlshinAF.Sprint5.Task0.V10.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint5.Task0.V10.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.AlshinAF.Sprint5.Task0.V10.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.GetTempFileName();
             double z = (1.6 * Math.Pow((double)x, 3) - 2.1 * Math.Pow((double)x, 2) + 7 * x);
             z = Math.Round(z, 3);
             File.WriteAllText(path, Convert.ToString(z));
