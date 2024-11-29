330bf0fc87994efee28de6f373a279af5099a953
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-30 01:43:48 +0500

    Task5

--- Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib/DataService.cs
@@ -10,12 +10,12 @@ namespace Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib
   
             double res = 0;
             double count = 0;
-            using (StreamReader reader = new StreamReader(value))
+            using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    double x = Convert.ToDouble(line);
+                    double x = Math.Round(Convert.ToDouble(line), 3);
                     if (x > 0)
                     {
                         count++;
