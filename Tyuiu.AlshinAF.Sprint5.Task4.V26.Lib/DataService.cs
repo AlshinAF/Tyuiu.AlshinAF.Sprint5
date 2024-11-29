c0438b26e1d82e48422c38ed4f2b0cb13da9e3cd
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-29 23:18:45 +0500

    Task4

--- Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib
         public double LoadFromDataFile(string path)
         {
             string srtX = File.ReadAllText(path);   
-            double res = Math.Round(Math.Pow(Convert.ToDouble(srtX), 3) + Math.Sin(Convert.ToDouble(srtX)) * Math.Cos(Convert.ToDouble(srtX)), 3);
+            double res = Math.Round(Math.Pow(Convert.ToDouble(srtX), 3) + Math.Sin(Convert.ToDouble(srtX)) * Math.Cos(Convert.ToDouble(srtX)),3);
             return res;
         }
     }
