52cbb59f7eebe2a0e9aa09326426288aeb727d8f
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-29 23:32:13 +0500

    Task4

--- Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib
         {
             string path1 = File.ReadAllText(path);
             string value = path1.Replace(".", ",");
-            string srtX = File.ReadAllText(path);   
+            string srtX = File.ReadAllText(value);   
             double res = Math.Round(Math.Pow(Convert.ToDouble(srtX), 3) + Math.Sin(Convert.ToDouble(srtX)) * Math.Cos(Convert.ToDouble(srtX)),3);
             return res;
         }
