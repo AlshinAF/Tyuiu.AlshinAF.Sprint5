ca87ed2dcb89cc1fdc91e801c319b668f1813d13
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-30 01:31:52 +0500

    Task5

---
+++ Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib
+{
+    public class DataService : ISprint5Task5V2
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            double count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (Convert.ToDouble(line) > 0)
+                    {
+                        count++;
+                        res = res + Convert.ToDouble(line);
+                    }
+
+                }
+
+            }
+            res = res / count;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AlshinAF.Sprint5.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+
+namespace Tyuiu.AlshinAF.Sprint5.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
