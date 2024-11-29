38954a24064fc7b6956d63ea51b0ab09dc29d050
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-29 23:10:41 +0500

    Task4

---
+++ Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.AlshinAF.Sprint5.Task4.V26.Lib
+{
+    public class DataService : ISprint5Task4V26
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string srtX = File.ReadAllText(path);   
+            double res = Math.Round(Math.Pow(Convert.ToDouble(strX), 3) + Math.Sin(Convert.ToDouble(srtX)) * Math.Cos(Convert.ToDouble(srtX)), 3);
+            return res;
+        }
+    }
+}
