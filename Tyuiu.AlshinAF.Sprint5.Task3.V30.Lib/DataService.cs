767e9e31c3a5900d23e7c2ad8692ff721e92e525
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-29 21:43:23 +0500

    Task3

--- Tyuiu.AlshinAF.Sprint5.Task3.V30.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint5.Task3.V30.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.AlshinAF.Sprint5.Task3.V30.Lib
         {
             string path = Path.GetTempFileName();
             double y = (Math.Pow(x, 3) - 1)/(4*x*x);
-            y = Math.Round(y, 2);
+            y = Math.Round(y, 3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
             {
                 writer.Write(BitConverter.GetBytes(y));
