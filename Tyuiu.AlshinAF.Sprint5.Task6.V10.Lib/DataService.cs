a7a89eb8b942071f53bf257c111013cbf0ec8406
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-30 02:07:17 +0500

    Task6

--- Tyuiu.AlshinAF.Sprint5.Task6.V10.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint5.Task6.V10.Lib/DataService.cs
@@ -1,19 +1,23 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Reflection.PortableExecutable;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.AlshinAF.Sprint5.Task6.V10.Lib
 {
     public class DataService : ISprint5Task6V10
     {
         public int LoadFromDataFile(string path)
         {
+            string s;
             int count = 0;
             using (StreamReader sr = new StreamReader(path))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
+                    
                     for (int i = 0; i < line.Length; i++)
                     {
                         string x = Convert.ToString(line[i]);
+                        x = x.Replace(",", "");
                         if (x.Length == 4)
                         {
                             count++;
