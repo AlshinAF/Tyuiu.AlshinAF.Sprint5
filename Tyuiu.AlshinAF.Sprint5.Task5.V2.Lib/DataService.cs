c7838da7698f55f9751c6f214ee5bbe3e67010c9
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-30 01:39:12 +0500

    Task5

--- Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.AlshinAF.Sprint5.Task5.V2.Lib
         public double LoadFromDataFile(string path)
         {
             string path1 = File.ReadAllText(path);
-            string value = path1.Replace(",", "");
+            string value = path1.Replace(",", " ");
             double res = 0;
             double count = 0;
             using (StreamReader reader = new StreamReader(value))
